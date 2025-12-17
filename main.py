50bb6c1de624a93a4e09425bc731a298f57e7d90
juancalheiros <juancalheiros0001@gmail.com>
2025-12-16 23:23:46 -0300

    feat: search jobs to many perfil

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import os
 import logging
 
-from json import dump as json_dump, dumps as json_dumps
+from json import dumps as json_dumps, dump as json_dump, loads as json_loads
 from google.cloud import pubsub_v1
 from dotenv import load_dotenv
 
@@ -20,12 +20,6 @@ headers = {
     "x-rapidapi-key" : os.getenv("API_KEY_RAPID"),
     "x-rapidapi-host": os.getenv("RAPID_API_HOST", "jsearch.p.rapidapi.com"),
 }
-query = {
-    "num_pages": int(os.getenv("NUM_PAGES", 3)),
-    "country": os.getenv("COUNTRY", "br"),
-    "data_posted":os.getenv("DATA_POSTED", "all"),
-    "work_from_home": os.getenv("REMOTE_JOB", "false"),
-}
 
 PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 TOPIC_ID = os.getenv("PUBSUB_TOPIC_ID")
@@ -42,44 +36,72 @@ def send_message_to_pubsub(data: str):
 
     logging.info(f"Published messages to {topic_path}.")
 
+
+def validation_envs() -> None:
+    required_vars = ["API_KEY_RAPID", "SEARCH_TARGETS", "GCP_PROJECT_ID", "PUBSUB_TOPIC_ID"]
+    empty_vars = ""
+    for var in required_vars:
+        if os.getenv(var) is None: 
+           empty_vars += f"{var}, "
+    if empty_vars:
+        raise Exception(f"🚨 Please add your {empty_vars} in .env\n")
+
 def search_job():
-    try: 
-        if not headers.get("x-rapidapi-key"):
-            raise Exception("🚨 Please add your API_KEY_RAPID in .env")
-        
-        terms_search = os.getenv("QUERY_SEARCH", "desenvolvedor fullstack").split(",")
+    try:
+        validation_envs()
         result = []
+        query = {
+            "num_pages": int(os.getenv("NUM_PAGES", 3)),
+            "country": os.getenv("COUNTRY", "br"),
+            "data_posted":os.getenv("DATA_POSTED", "all"),
+            "work_from_home": os.getenv("REMOTE_JOB", "false"),
+        }
+        search_targets = json_loads(os.getenv("SEARCH_TARGETS", "[]"))
 
-        for job_search in terms_search:
-            query["query"] = job_search
+        for target in search_targets:
+            jobs = []
+            email = target.get("email")
 
-            logging.info(f"Initiating job search request... {job_search}")
-            response  = requests.get(url, headers=headers, params=query)
-            data  = response.json()
-            logging.info("Request sucess. Processing data...")
+            keywords = target.get("keywords", "desenvolvedor backend").split(",")
+            partial_result = []
 
-            for job_data in data.get("data"):
-                result.append({
-                    "job_id": job_data.get("job_id"),
-                    "job_title": job_data.get("job_title"),
-                    "company": job_data.get("employer_name"), 
-                    "job_apply_link": job_data.get("job_apply_link"),
-                    "description": job_data.get("job_description"),
-                    "job_is_remote": job_data.get("job_is_remote"),
-                    "job_location": job_data.get("job_location"),
-                    "job_google_link": job_data.get("job_google_link"),
-                    "apply_options": job_data.get("apply_options"),
-                })
+            for job_search in keywords:
+                params = {**query, "query": job_search}
+
+                logging.info(f"Initiating job search request... {job_search}")
+                response  = requests.get(url, headers=headers, params=params)
+                response.raise_for_status()
+
+                data = response.json()
+
+                logging.info("Request sucess. Processing data...") 
+                for job_data in data.get("data"):
+                    jobs.append({
+                        "job_id": job_data.get("job_id"),
+                        "job_title": job_data.get("job_title"),
+                        "company": job_data.get("employer_name"), 
+                        "job_apply_link": job_data.get("job_apply_link"),
+                        "description": job_data.get("job_description"),
+                        "job_is_remote": job_data.get("job_is_remote"),
+                        "job_location": job_data.get("job_location"),
+                        "job_google_link": job_data.get("job_google_link"),
+                        "apply_options": job_data.get("apply_options"),
+                    })
+                partial_result.append({"keywords": job_search, "jobs": jobs})
+
+            result.append({"email": email, "searches": partial_result})
 
             with open("vagas.json", "w", encoding="utf-8") as f:
                 json_dump(result, f, indent=4, ensure_ascii=False)
 
-            logging.info(f"Finish process, total: {len(result)} jobs found.")
+            logging.info(f"Finish process. Found {len(jobs)} jobs for {keywords}")
             logging.debug(f"Result data: {result}")
 
             send_message_to_pubsub(data=json_dumps(result).encode("utf-8"))
 
         return result
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
